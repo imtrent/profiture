e70bd9ff69e44ed8b55c5333b4536f5f2462b8f3
Ian <ian.m.trent@gmail.com>
2019-06-11 22:18:14 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
---
+++ src/components/ExpenseDashboardPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ExpenseDashboardPage = () => (
+	<div>
+		<p>This is form my dashboard component</p>
+	</div>
+);
+
+export default ExpenseDashboardPage;
