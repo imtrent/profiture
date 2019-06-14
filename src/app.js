4c427956885e18524e883a2e0c2aa9170600193f
Ian <ian.m.trent@gmail.com>
2019-06-13 21:22:03 -0400

    add: Setup Redux actions functionality

--- src/app.js
+++ src/app.js
@@ -8,4 +8,14 @@ import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Laptop' , amount: 180000, createdAt: 1497657600 }));
+store.dispatch(addExpense({ description: 'Internet', amount: 5000, createdAt: 1561939200 }));
+store.dispatch(setTextFilter('Int'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
