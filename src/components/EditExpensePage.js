508064d5d25e1028bbf630d1c9d4de41086fd725
Ian <ian.m.trent@gmail.com>
2019-06-22 18:10:04 -0400

    add: Edit expense functionality

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { editExpense, removeExpense } from '../actions/expenses';
+import ExpenseForm from './ExpenseForm';
 
 const EditExpensePage = (props) => {
 	console.log(props);
 	return (
 		<div>
-			<p>Edit expense page with the id of {props.match.params.id}</p>
+			<ExpenseForm
+				expense={props.expense}
+				onSubmit={(expense) => {
+					props.dispatch(editExpense(props.expense.id, expense));
+					props.history.push('/');
+					console.log('updated', expense);
+				}}
+			/>
+			<button onClick={(e) => {
+				props.dispatch(removeExpense({ id: props.expense.id }));
+				props.history.push('/');
+			}}>Delete Expense</button>
 		</div>
 	);
 };
 
-export default EditExpensePage;
+const mapStateToProps = (state, props) => {
+	return {
+		expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+	};
+};
+
+export default connect(mapStateToProps)(EditExpensePage);
